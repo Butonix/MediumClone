411753dbc256faad05aa0eb76bf47bf679006547
Jesus Celemin <jesuscelemin.s@gmail.com>
2017-05-08 12:29:03 +0200

    Add mongoose

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost:27017/medium', (err, res) => {
+  if (err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log('Conexión a la base de datos establecida correctamente...')
+})
 
 app.listen(port, () => {
   console.log(`API Rest corriendo en http://localhost:${port}`)
